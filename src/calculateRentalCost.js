2844b59a0c36e5345550f7ac720d76d505558541
ArtemYukhymenko <winner.wcar@gmail.com>
2024-11-12 21:54:32 +0200

    Revert "Solution"

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,23 +4,7 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const PRICE_PER_DAY = 40;
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-  const SHORT_TERM = 3;
-  const SHORT_TERM_DISCOUNT = 20;
-
-  const totalCost = days * PRICE_PER_DAY;
-
-  if (days >= LONG_TERM) {
-    return totalCost - LONG_TERM_DISCOUNT;
-  }
-
-  if (days >= SHORT_TERM) {
-    return totalCost - SHORT_TERM_DISCOUNT;
-  }
-
-  return totalCost;
+  // write code here
 }
 
 module.exports = calculateRentalCost;
